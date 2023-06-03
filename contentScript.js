2593b108dce6ebf1695e3ef50d9f251ab9eba061
Jean-Marc Clement <jmclement16@gmail.com>
2023-06-03 04:55:05 +0400

    Additional data

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,6 @@
 let bToken = "";
+let elgRisk = "";
+let elgAmount = 0;
 
 const init = function(){
   
@@ -6,10 +8,40 @@ const init = function(){
 
   for(i=0;i<cards.length;i++){
     const iEl = document.createElement('img');
+    let price = 0;
     iEl.src = 'https://www.mcb.mu/resource/mcb/images/logo.png';
     iEl.width = 50;
     iEl.height = 50;
     cards[i].insertAdjacentElement('beforebegin', iEl);
+    parentEl = cards[i].parentNode;
+    parentEl = parentEl.parentNode;
+    priceEl = parentEl.querySelector('.price');
+
+    if(priceEl != null) {
+      if(priceEl.firstChild.type != undefined) {
+        priceAEl = priceEl.getElementsByTagName('a');
+        price = priceAEl[0].innerText;
+      } else {
+        price = priceEl.innerText;
+      }
+    }
+
+    if (price != 0) {
+      price = price.replaceAll('Rs ','').replaceAll(',','');
+    }
+    
+    iPriceEl = document.createElement('div');
+
+    if (price <= elgAmount) {
+      iPriceEl.innerText = "Eligible";
+    }
+    else {
+      iPriceEl.innerText = "Not Eligible";
+    }
+
+
+    cards[i].insertAdjacentElement('beforebegin',iPriceEl);
+
   }
 
   apiAuth();
@@ -19,7 +51,7 @@ const init = function(){
 const apiAuth = async function(){
   url = "https://mcbinovappapi.azurewebsites.net/auth";
   let reqBody = {
-    "username": "hack124565",
+    "username": "HW654321",
     "password": "test"
   }
   response = await fetch(url, {method: 'POST', body: JSON.stringify(reqBody)});
@@ -41,10 +73,10 @@ const apiEligibilityCheck = async function(){
   }
 
   const url = "https://mcbinovappapi.azurewebsites.net/loans/eligibilityCheck";
-  console.log(headers);
-  console.log(reqBody);
   response = await fetch(url,{method: 'POST', headers: headers, body: JSON.stringify(reqBody)});
-  console.log(await response.json())
+  elgData = await response.json();
+  elgRisk = elgData['Risk'];
+  elgAmount = elgData['LoanEligibleAmount'];
 }
 
 init();
